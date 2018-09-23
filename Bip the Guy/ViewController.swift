c7f314d0dd1bea086ec00b22b6304dc95930dc05
Jacqueline Han <jacquelinehan@jacquelines-mbp.bc.edu>
2018-09-23 19:10:14 -0400

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Jacqueline Han on 9/23/18.
+//  Copyright © 2018 Jacqueline Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Bip the GuyTests/Bip_the_GuyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bip_the_GuyTests.swift
+//  Bip the GuyTests
+//
+//  Created by Jacqueline Han on 9/23/18.
+//  Copyright © 2018 Jacqueline Han. All rights reserved.
+//
+
+import XCTest
+@testable import Bip_the_Guy
+
+class Bip_the_GuyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
