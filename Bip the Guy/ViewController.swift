5746ab977e228202b9c05e2a45c096fa7735dcb6
Jacqueline Han <jacquelinehan@jacquelines-mbp.bc.edu>
2018-09-23 23:30:54 -0400

    4.1

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageToPunch: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey! You just pressed the image!")
+    }
+    
+    
+    
 }
 
